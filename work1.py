e3a1f2d897c3afb51446f61657afb35e22e394a4
ksard <flastdtb@yandex.ru>
2024-07-24 15:35:41 +0500

    finish

--- work1.py
+++ work1.py
@@ -1,8 +1,21 @@
+#!/usr/bin/python3
+from typing import Dict, Any
+
+# В ячейке ниже представлен код генерирующий DataFrame, которая состоит всего из 1 столбца. Ваша задача перевести его
+# в one hot вид. Сможете ли вы это сделать без get_dummies?
 import pandas as pd
 import random
+
 lst = ['robot'] * 10
 lst += ['human'] * 10
 random.shuffle(lst)
-data = pd.DataFrame({'whoAmI':lst})
+data = pd.DataFrame({'whoAmI': lst})
 data.head()
-print(data)
+
+for col in data:
+    columns = data['whoAmI'].unique()
+
+lastDate = pd.DataFrame()
+for col in columns:
+    lastDate[col] = data['whoAmI'].apply(lambda x: 1 if x == col else 0)
+print(lastDate)
